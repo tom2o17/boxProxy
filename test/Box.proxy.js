411984691f5d7b32af852b66508a35234b6dba1e
Thomas Clement <Thomasaclement98@gmail.com>
2021-12-20 13:58:19 -0500

    Update Box.proxy.js

--- test/Box.proxy.js
+++ test/Box.proxy.js
@@ -13,5 +13,8 @@ describe('Box (proxy)', function () {
       // Test if the returned value is the same one
       // Note that we need to use strings to compare the 256 bit integers
       expect((await box.retrieve()).toString()).to.equal('42');
+
+      // TODO add more test cases and check out how to implement this with different function
+      
     });
   });
