7dc3afdd7df20afe0f9cce66dee7c0490ef7b64e
Thomas Clement <Thomasaclement98@gmail.com>
2021-12-23 20:33:24 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require('@openzeppelin/hardhat-upgrades');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
+// 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
